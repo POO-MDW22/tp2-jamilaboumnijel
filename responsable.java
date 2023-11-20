f080240d9fac0a9e166d1896a627228f4f5e7ffe
jamilaboumnijel <147211314+jamilaboumnijel@users.noreply.github.com>
2023-11-20 11:42:42 +0100

    Delete responsable.java

--- responsable.java
+++
@@ -1,7 +0,0 @@
-public class responsable extends employee{
-   int prime;
-    public responsable(String nom,int id,String adrs,int nb_h,int prime){
-        super(nom,id,adrs,nb_h);
-        this.prime=prime;
-    }
-}
